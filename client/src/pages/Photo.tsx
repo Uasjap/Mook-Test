f881a66295571a0cdaba302ea577c641ca40032b
Uasjap <uasjap@gmail.com>
2024-04-17 12:21:32 +0300

    bugfix

--- client/src/pages/Photo.tsx
+++ client/src/pages/Photo.tsx
@@ -2,7 +2,7 @@
 export default function Photo():JSX.Element {
   return (
     <div className='photoContainer'>
-        <img className='thePhoto' src='src\img\selfy_by_crayolajustgotbetter_d6lk534-pre.jpg' alt='img'/>
+        <img className='thePhoto' src='../img/selfy_by_crayolajustgotbetter_d6lk534-pre.jpg' alt='img'/>
         <div className='Lego'>Lego</div>
       </div>
   )
